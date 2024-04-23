e4602a8debe570f7feccca0433df3bb41a96ce04
Ubbie <ubbiehawk@gmail.com>
2024-04-23 15:35:56 -0400

    Updated Checkout and Recipt Screen
    I started on the receipt page and gave an confirmation of an order as well as trying to implement a timer for eta

--- food_delivery_app/lib/checkout.dart
+++ food_delivery_app/lib/checkout.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -5,92 +6,113 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/widgets.dart';
 import 'firebase_options.dart';
 import 'package:food_delivery_app/main.dart';
-
+import 'package:food_delivery_app/receipt.dart';
 
 late final CollectionReference _orders = FirebaseFirestore.instance.collection('order');
-class CartScreen extends StatelessWidget{
 
-Widget build(BuildContext context) {
+class CartScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double total = 0;
+
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Cart Checkout'),  
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Divider(
-              height: 30,
-              thickness: 5,
-              color: Colors.black,
-            ),
-            const Text('Current Order', style: TextStyle(fontSize: 30)),
-            Expanded(
-              child: StreamBuilder(
-                  stream: _orders.snapshots(),
-                  builder:
-                      (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
-                    if (streamSnapshot.hasData) {
-                      return ListView.builder(
-                        itemCount: streamSnapshot.data!.docs.length,
-                        itemBuilder: (context, index) {
-                          final DocumentSnapshot documentSnapshot =
-                              streamSnapshot.data!.docs[index];
-                          return Card(
-                              margin: const EdgeInsets.all(10),
-                              child: ListTile(
-                                title: Text(
-                                    documentSnapshot['name'] +
-                                        '  --  \$${documentSnapshot['price']}',
-                                    style: const TextStyle(fontSize: 14)),
-                               
-                              ));
-                        },
-                      );
-                    }
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }),
-            ),
-            const Divider(
-              height: 30,
-              thickness: 5,
-              color: Colors.black,
-            ),
-            Center(       
-      child: Row(
+      appBar: AppBar(
+        title: Text('Cart Checkout'),
+      ),
+      body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-           const Text('Total Price: Total Here', style: TextStyle(fontSize: 30), textAlign: TextAlign.center,),
-          
-        ],
-      ),    
-              
+          const Divider(
+            height: 30,
+            thickness: 5,
+            color: Colors.black,
+          ),
+          const Text('Current Order', style: TextStyle(fontSize: 30)),
+          Expanded(
+            child: StreamBuilder(
+              stream: _orders.snapshots(),
+              builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+                if (streamSnapshot.hasData) {
+                  // Calculate total price
+                  // total = streamSnapshot.data!.docs.fold<double>(
+                  //   0,
+                  //   (previousValue, doc) => previousValue + doc['price'],
+                  // );
+
+                  return ListView.builder(
+                    itemCount: streamSnapshot.data!.docs.length,
+                    itemBuilder: (context, index) {
+                      final DocumentSnapshot documentSnapshot = streamSnapshot.data!.docs[index];
+                      return Card(
+                        margin: const EdgeInsets.all(10),
+                        child: ListTile(
+                          title: Text(
+                            documentSnapshot['name'] + '  --  \$${documentSnapshot['price']}',
+                            style: const TextStyle(fontSize: 14),
+                          ),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.delete),
+                            onPressed: () {
+                              _orders.doc(documentSnapshot.id).delete();
+                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Item Deleted From Cart')));
+                            },
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                }
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              },
             ),
-        SizedBox(height: 60,),
-           ElevatedButton(onPressed: (){
-            _clearCurrentOrder();
-           }, 
-           child: Text('Check Out', style: TextStyle(fontSize:25),),
-             style: ElevatedButton.styleFrom(
-    minimumSize: Size(200, 50),
-    foregroundColor: Colors.white,
-    backgroundColor: Color.fromARGB(255, 10, 10, 10), // Change the width and height here
-  ), ),
-   SizedBox(height: 130,),
-           
-          ],
-        ));
+          ),
+          const Divider(
+            height: 30,
+            thickness: 5,
+            color: Colors.black,
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Total Price:', style: TextStyle(fontSize: 30)),
+                Text(
+                  '\$$total',
+                  style: TextStyle(fontSize: 30),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 60),
+          ElevatedButton(
+            onPressed: () {
+             // _clearCurrentOrder();
+             Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ConfirmationScreen()));
+            },
+            child: Text('Check Out', style: TextStyle(fontSize: 25)),
+            style: ElevatedButton.styleFrom(
+              minimumSize: Size(200, 50),
+              foregroundColor: Colors.white,
+              backgroundColor: Color.fromARGB(255, 10, 10, 10),
+            ),
+          ),
+          SizedBox(height: 130),
+        ],
+      ),
+    );
   }
 
-void _clearCurrentOrder() {
+  void _clearCurrentOrder() {
     // Clear current order data
     _orders.get().then((snapshot) {
       for (DocumentSnapshot ds in snapshot.docs) {
         ds.reference.delete();
       }
     });
-
   }
 }
-
